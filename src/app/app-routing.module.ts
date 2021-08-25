993c099fc6a6b2e0b62ac01a40766f9ce7a92cba
pavan62 <pavankamath142@gmail.com>
2021-08-25 12:08:08 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlackComponent } from './black/black.component';
+import { WhiteComponent } from './white/white.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',redirectTo:'black',pathMatch:'full'
+}
+  ,{
+
+  path:'black',component:BlackComponent
+},
+{
+  path:'white',component:WhiteComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlackComponent } from './black/black.component';
+import { WhiteComponent } from './white/white.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackComponent,
+    WhiteComponent
   ],
   imports: [
     BrowserModule,
